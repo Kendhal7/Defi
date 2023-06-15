7213e428c404245a5d8ec500da6e787da0ecb400
Kendhal Altay <47398719+Kendhal7@users.noreply.github.com>
2023-06-15 14:44:36 +0200

    update: - addresses.yaml contains all the addresses that needs to be processed by wallet_link.py

--- wallet_link.py
+++ wallet_link.py
@@ -1,5 +1,6 @@
 import requests
 import yaml
+import time
 
 # Upload the config file
 file = open(f"credentials.yml", 'r')
@@ -9,15 +10,38 @@ config = yaml.load(file, Loader=yaml.FullLoader)
 ETHERSCAN_API_KEY = config['Etherscan']["API_KEY"]
 API_URL = "https://api.etherscan.io/api?module=account&action=txlist&address={}&startblock=0&endblock=99999999&sort=asc&apikey={}"
 
+# Upload the addresses file
+with open("addresses.yaml", 'r') as file:
+    addresses = yaml.load(file, Loader=yaml.FullLoader)
 
-def get_transactions(address):
+# Get Addresses
+TARGET_ADDRESSES = {address.lower(): name for address, name in addresses['TARGET_ADDRESSES'].items()}
+START_ADDRESSES = {address.lower(): name for address, name in addresses['START_ADDRESSES'].items()}
+EXCLUDE_ADDRESSES = [address.lower() for address in addresses['EXCLUDE_ADDRESSES']]
+
+
+def get_transactions(address, retries=3, delay=5):
     url = API_URL.format(address, ETHERSCAN_API_KEY)
-    response = requests.get(url)
-    data = response.json()
-    return data['result'] if 'result' in data else []
+    for i in range(retries):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # raises an HTTPError if one occurred
+            data = response.json()
+            return data['result'] if 'result' in data else []
+        except requests.HTTPError as e:
+            if i < retries - 1:  # i is zero indexed
+                time.sleep(delay)  # wait before trying again
+                continue
+            else:
+                print(f"Failed to fetch transactions for address {address} after {retries} attempts.")
+                raise e
 
 
-def find_hops(start_address, target_addresses, max_hops=5, max_transactions=100):
+def find_hops(start_address, target_addresses, exclude_addresses=None, max_hops=2, max_transactions=100):
+    if exclude_addresses is None:
+        exclude_addresses = set()
+    else:
+        exclude_addresses = set([address.lower() for address in exclude_addresses])
     visited = set()
     queue = [(start_address.lower(), [], None, 0)]
     enqueued = set([start_address.lower()])
@@ -26,11 +50,12 @@ def find_hops(start_address, target_addresses, max_hops=5, max_transactions=100)
         address, path, prev_tx_hash, hops = queue.pop(0)
         path = path + [(address, prev_tx_hash)]
         transactions = get_transactions(address)
+        print(path)
         for transaction in transactions:
             new_addresses = [transaction['to'].lower(), transaction['from'].lower()]
             tx_hash = transaction['hash']
             for new_address in new_addresses:
-                if new_address and new_address not in visited and new_address not in enqueued:
+                if new_address and new_address not in visited and new_address not in enqueued and new_address not in exclude_addresses:
                     if new_address in target_addresses.keys():
                         paths.append((path + [(new_address, tx_hash)], hops + 1))
                     elif hops < max_hops and len(transactions) <= max_transactions:
@@ -41,33 +66,12 @@ def find_hops(start_address, target_addresses, max_hops=5, max_transactions=100)
 
 
 def main():
-    start_addresses = {
-        '0x50eD4D3e48B27681371b0c9F375Eb12a85e241Dc': 'TEST_1',
-        '0x58203347923ef9751748D098084611FF4473d1Cd': 'TEST_4',
-        '0xbdb4BeeF21efC8AE04A6Bf11e685954BEc015125': 'TEST_5',
-        '0x80C94d637F5F51758D5935284F3B3091ceEf2a8C': 'TEST_6',
-        '0x7322f9932d68FB99a84fb9F89375c8cB7EBb9bB9': 'TEST_7',
-        '0x32Fd0f2853dd29b479c4879D7683d81e5C3cC3c1': 'TEST_8',
-        '0x4730d108aC076973373155932a465ed438C7b1a2': 'TEST_9',
-        '0x82ea7840441c4B4E16Ce4A9c58364dc7FBe19048': 'TEST_10',
-        '0x861193dF5007fDE8DC8F2B72dB746DFf226Cea68': 'TEST_11',
-        '0x743CB013D459c358ed571A21cC105E35c284C385': 'TEST_12',
-        '0x9822E23558c2837a499541Bf22433B0F820F213A': 'TEST_13',
-        '0x5440cC69CB31CA46decf34C246FC395378D731b4': 'TEST_14',
-        '0xC78d8F4493B5A1455152DE575deE50D986871eC9': 'TEST_15',
-    }
-
-    start_addresses = {address.lower(): name for address, name in start_addresses.items()}
+    start_addresses = {address.lower(): name for address, name in addresses['START_ADDRESSES'].items()}
+    target_addresses = {address.lower(): name for address, name in addresses['TARGET_ADDRESSES'].items()}
+    exclude_addresses = [address.lower() for address in addresses['EXCLUDE_ADDRESSES']]
 
     for start_address, start_name in start_addresses.items():
-        target_addresses = {
-            '0x45dd43D67C4dC159017F14faCa48D231d3616fac': 'POE',
-            '0xc9E170d9C62b7765F624459C3fdDf23de9f4CeC3': 'CTM',
-            '0xd6BDF425640032b949aeB2130a9ACB9a3181B58b': 'WJS',
-            '0x58203347923ef9751748D098084611FF4473d1Cd': 'TEST_4',
-        }
-        target_addresses = {address.lower(): name for address, name in target_addresses.items()}
-        paths = find_hops(start_address, target_addresses)
+        paths = find_hops(start_address, target_addresses, exclude_addresses)
         if paths:
             for path, hops in paths:
                 print(
