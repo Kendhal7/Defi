867e52d167da33158f11b920810a8c38f89fcfcd
Kendhal Altay <47398719+Kendhal7@users.noreply.github.com>
2023-06-23 11:42:29 +0200

    update: - FixedFloat with postgreSQL, and error catching

--- FixedFloat.py
+++ FixedFloat.py
@@ -1,16 +1,24 @@
-import json
-import os
 from datetime import datetime, timedelta
-from typing import List
-
-import requests
-import yaml
+from dataclasses import dataclass
+from operator import getitem
+from functools import reduce
+from pathlib import Path
+from typing import List, Dict
+from psycopg2 import sql
 from tqdm import tqdm
+import psycopg2
+import requests
+import schedule
+import yaml
+import json
+import time
 
 
+@dataclass
 class Config:
-    def __init__(self, file_path: str) -> None:
-        self.file_path = file_path
+    file_path: str
+
+    def __post_init__(self):
         self.data = self.load_config()
 
     def load_config(self) -> dict:
@@ -18,27 +26,141 @@ class Config:
             return yaml.safe_load(file)
 
     def get_value(self, key: str) -> str:
-        keys = key.split('.')
-        value = self.data
-        for k in keys:
-            value = value[k]
-        return value
+        """Get a nested value from the config file using a dot-separated string."""
+        return reduce(getitem, key.split('.'), self.data)
 
 
+class TelegramAlert:
+
+    def __init__(self):
+        config = Config("credentials.yml")
+        self.token_id = config.get_value('Telegram.TOKEN_ID')
+        self.chat_id = config.get_value('Telegram.CHAT_ID')
+
+    def send_telegram_message(self, message):
+        """Sends message via Telegram"""
+
+        url = "https://api.telegram.org/bot" + self.token_id + "/sendMessage"
+        data = {
+            "chat_id": self.chat_id,
+            "text": message
+        }
+
+        try:
+            response = requests.request("POST", url, params=data)
+            telegram_data = json.loads(response.text)
+            return telegram_data["ok"]
+        except Exception as e:
+            print("An error occurred in sending the alert message via Telegram")
+            print(e)
+        return False
+
+
+@dataclass
+class CryptoInfo:
+    api_key: str
+    exclude = ["github.com", "proofpatform.io", "zeppelin", "instagram.com", "dapp.tools", "solidity", "eips.ethereum",
+               "eth.wiki", "nomic-labs-blog", "etherscan", "Etherscan", 'tokenmint.io', 'hardhat']
+
+    @staticmethod
+    def clean_string(s: str) -> str:
+        """Clean string by removing unwanted characters."""
+        if '\\' in s:
+            s = s.split('\\')[0]
+        if s.startswith("-https://"):
+            s = s.replace("-https://", "https://", 1) # replace only the first occurrence
+        return s
+
+    def scrap_contract_links(self, contract_address) -> Dict[str, str]:
+        try:
+            url = f"https://api.etherscan.io/api?module=contract&action=getsourcecode&apikey={self.api_key}&address={contract_address.lower()}"
+            response = requests.get(url)
+            response.raise_for_status()
+
+            links = {}
+            for info in response.json()["result"]:
+                for word in info['SourceCode'].split():
+                    if word.startswith("-http"):
+                        word = word[1:]  # remove leading '-'
+                    if word.startswith("http"):
+                        if "twitter.com" in word:
+                            links["twitter"] = word
+                        elif "telegram.me" in word or "t.me" in word:
+                            links["telegram"] = word
+                        else:
+                            if not any(substring in word for substring in self.exclude):
+                                links["website"] = word
+                    else:
+                        if "twitter.com" in word:
+                            links["twitter"] = "https://" + word
+                        elif "telegram.me" in word or "t.me" in word:
+                            links["telegram"] = "https://" + word
+                        else:
+                            if ".io" in word:
+                                if not any(substring in word for substring in self.exclude):
+                                    links["website"] = "https://" + word
+            return links
+        except Exception as e:
+            return None
+
+    def scrap_dexscreener(self, contract_address) -> Dict[str, str]:
+        try:
+            url = f"https://api.dexscreener.com/latest/dex/tokens/{contract_address.lower()}"
+            response = requests.get(url)
+            response.raise_for_status()
+
+            info = response.json()['pairs'][0]
+            infos = {"name": info['baseToken']['name'],
+                     "symbol": info['baseToken']['symbol'],
+                     "mc": self.format_number(info['fdv']),
+                     "usd_price": info['priceUsd'],
+                     "volume": self.format_number(info['volume']['h24']),
+                     "priceChange": info['priceChange']['h24'],
+                     "creation": info['pairCreatedAt']}
+            return infos
+        except Exception as e:
+            return None
+
+    @staticmethod
+    def format_number(num: float) -> str:
+        if num >= 1_000_000:
+            return f"{round(num / 1_000_000, 1)}M"
+        elif num >= 1_000:
+            return f"{round(num / 1_000, 1)}K"
+        else:
+            return str(num)
+
+    def create_and_print_message(self, contract_address):
+        links = self.scrap_contract_links(contract_address)
+        infos = self.scrap_dexscreener(contract_address)
+
+        message = f"{infos['name']} - ${infos['symbol']} - {infos['mc']} MC\n" \
+                  f"\n📩 {links.get('telegram', 'Not available')}\n" \
+                  f"🐦 {links.get('twitter', 'Not available')}\n" \
+                  f"🌐 {links.get('website', 'Not available')}\n" \
+                  f"📈 https://dexscreener.com/ethereum/{contract_address}\n\n" \
+                  f"Additional Information:\n" \
+                  f"Price USD: ${infos['usd_price']}\n" \
+                  f"Volume 24H: ${infos['volume']}\n" \
+                  f"Price Change 24H: {infos['priceChange']}%\n" \
+                  f"Pair Created: {datetime.fromtimestamp(int(infos['creation'] / 1000)) - timedelta(hours=1)} UTC"
+        return message
+
+
+@dataclass
 class EtherscanAPI:
+    api_key: str
     WEI_TO_ETHER = 10 ** 18
-
-    def __init__(self, api_key: str) -> None:
-        self.api_key = api_key
 
     def get_transactions(self, address: str) -> List[dict]:
         url = f"https://api.etherscan.io/api?module=account&action=txlist&address={address}&startblock=0&endblock=99999999&sort=desc&apikey={self.api_key}"
         response = requests.get(url)
-        data = response.json()
-        return data.get('result', [])
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('result', [])
+        response.raise_for_status()
 
-    def was_address_active_before(self, address: str, timestamp: datetime, txhash: str) -> bool:
-        transactions = self.get_transactions(address)
+    def was_address_active_before(self, transactions: List[dict], timestamp: datetime, txhash: str) -> bool:
         for tx in transactions:
             if tx['hash'] == txhash:
                 continue
@@ -47,25 +169,133 @@ class EtherscanAPI:
                 return True
         return False
 
-    def did_address_create_contract(self, address: str) -> bool:
-        transactions = self.get_transactions(address)
+    def did_address_create_contract(self, transactions: List[dict]) -> bool:
+        return any(isinstance(tx, dict) and tx['to'] == '' for tx in transactions)
+
+    def did_address_swap(self, transactions: List[dict]) -> bool:
+        db_manager = DBManager(db_name='kendhalaltay', user='kendhalaltay')
+        processed_transactions = set(db_manager.get_all_transactions())  # Convert to set for quicker lookup
+
+        functions_to_check = ['swapExactETHForTokens', 'unoswap', 'execute', 'swap']
+        uniswap_router_address = "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD".lower()
+
         for tx in transactions:
-            # A contract creation transaction has its `to` field as null
-            # and `input` field contains the contract creation code
-            if tx['to'] == '':  # `input` is '0x' for non-contract creation transactions
-                return True
+            if isinstance(tx, dict) and (('functionName' in tx and any(func in tx['functionName'] for func in functions_to_check)) or tx.get('to', '').lower() == uniswap_router_address):
+                if tx['hash'] not in processed_transactions:
+                    telegram_alert = TelegramAlert()
+                    telegram_alert.send_telegram_message(
+                        f"A swap was performed, here's the link: https://etherscan.io/tx/{tx['hash']}")
+                    try:
+                        crypto_info = CryptoInfo(self.api_key)
+                        for token in self.get_token_address(tx['hash']):
+                            try:
+                                telegram_alert.send_telegram_message(f"{crypto_info.create_and_print_message(token)}")
+                            except TypeError as e:
+                                continue
+                    except IndexError as e:
+                        continue
+                    db_manager.insert_transaction(tx['hash'])
+                    db_manager.close_connection()
+                    return True
+
+        db_manager.close_connection()  # Close the connection when done
         return False
+
+    def get_balance(self, address: str) -> float:
+        url = f"https://api.etherscan.io/api?module=account&action=balance&address={address}&tag=latest&apikey={self.api_key}"
+        response = requests.get(url)
+        if response.status_code != 200:
+            response.raise_for_status()
+        data = response.json()
+        return int(data.get('result', 0)) / self.WEI_TO_ETHER
+
+    def get_token_address(self, txhash: str):
+        known_coins = [
+            '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+        ]
+        addresses = set()
+        url = f"https://api.etherscan.io/api?module=proxy&action=eth_getTransactionReceipt&txhash={txhash}&apikey={self.api_key}"
+        response = requests.get(url)
+        if response.status_code != 200:
+            response.raise_for_status()
+        data = response.json()
+        for i in data.get('result')['logs']:
+            if i.get('address').lower() not in [coin.lower() for coin in known_coins]:
+                addresses.add(i.get('address'))
+        return addresses
 
 
 class FileManager:
     @staticmethod
     def create_dir(path: str) -> None:
-        os.makedirs(path, exist_ok=True)
+        Path(path).mkdir(parents=True, exist_ok=True)
 
     @staticmethod
     def write_to_file(file_path: str, data: dict) -> None:
         with open(file_path, 'w') as f:
             json.dump(data, f, indent=2)
+
+
+class DBManager:
+    def __init__(self, db_name, user, password=None, host="localhost"):
+        self.conn = psycopg2.connect(dbname=db_name, user=user, password=password, host=host)
+        self.c = self.conn.cursor()
+        self.c.execute('''CREATE TABLE IF NOT EXISTS addresses
+                     (address text PRIMARY KEY)''')
+        self.c.execute('''CREATE TABLE IF NOT EXISTS transactions
+                             (txhash text PRIMARY KEY)''')
+        self.conn.commit()
+
+    def insert_address(self, address):
+        try:
+            self.c.execute(sql.SQL("INSERT INTO addresses (address) VALUES (%s)"), (address,))
+        except psycopg2.IntegrityError:
+            self.conn.rollback()  # address already exists in the database
+        else:
+            self.conn.commit()
+
+    def insert_transaction(self, txhash):
+        try:
+            self.c.execute(sql.SQL("INSERT INTO transactions (txhash) VALUES (%s)"), (txhash,))
+            self.conn.commit()  # Commit changes
+        except psycopg2.IntegrityError:
+            self.conn.rollback()  # transaction already exists in the database
+
+    def get_all_addresses(self):
+        self.c.execute('SELECT address FROM addresses')
+        return [record[0] for record in self.c.fetchall()]
+
+    def get_all_transactions(self):
+        self.c.execute('SELECT txhash FROM transactions')
+        return [record[0] for record in self.c.fetchall()]
+
+    def remove_address(self, address: str):
+        self.c.execute("DELETE FROM addresses WHERE address=%s", (address,))
+        self.conn.commit()
+
+    def close_connection(self):
+        self.conn.close()
+
+
+def check_swaps():
+    db = DBManager(db_name='kendhalaltay', user='kendhalaltay')
+    addresses = db.get_all_addresses()
+
+    etherscan = EtherscanAPI(Config("credentials.yml").get_value('Etherscan.API_KEY'))
+
+    addresses_with_swap = []
+    for address in tqdm(addresses, desc="Checking swaps"):
+        transactions = etherscan.get_transactions(address)
+        if etherscan.did_address_swap(transactions):
+            addresses_with_swap.append(address)
+        elif etherscan.did_address_create_contract(transactions):
+            db.remove_address(address)
+        elif etherscan.get_balance(address) < 0.1:
+            db.remove_address(address)
+
+    print(f"\nAddresses that performed a swap: {addresses_with_swap}")
+
+    db.close_connection()
 
 
 def main():
@@ -83,15 +313,16 @@ def main():
     transactions = etherscan.get_transactions(ADDRESS)
 
     filtered_transactions = []
-    print(datetime.now())
-    time_threshold = datetime.now() - timedelta(hours=5)
+    print('------------------------------------------------------------------')
+    print('\nTIME:', datetime.now(), '\n')
+    time_threshold = datetime.now() - timedelta(minutes=30)
 
     # Filter transactions
     for tx in transactions:
         tx_time = datetime.utcfromtimestamp(int(tx['timeStamp']))
         if tx['from'].lower() == ADDRESS.lower() and tx_time >= (time_threshold - timedelta(hours=2)):
             value_ether = int(tx['value']) / EtherscanAPI.WEI_TO_ETHER
-            if 0.5 <= value_ether <= 5:
+            if 0.5 <= value_ether <= 10:
                 filtered_transactions.append(tx)
 
     # Filter addresses with no activity before the transaction from the target wallet
@@ -100,27 +331,37 @@ def main():
         address = tx['to']
         tx_time = datetime.utcfromtimestamp(int(tx['timeStamp']))
         tx_hash = tx['hash']
-        if not etherscan.was_address_active_before(address, tx_time, tx_hash):
+        transactions = etherscan.get_transactions(address)
+        if not etherscan.was_address_active_before(transactions, tx_time, tx_hash):
             addresses_with_no_prior_activity.append(address)
-
-    print(addresses_with_no_prior_activity)
 
     # Filter addresses that created contracts
     addresses_without_contracts = []
     for address in tqdm(addresses_with_no_prior_activity, desc="Checking contract creation"):
-        if not etherscan.did_address_create_contract(address):
+        transactions = etherscan.get_transactions(address)
+        if not etherscan.did_address_create_contract(transactions):
             addresses_without_contracts.append(address)
 
-    print(addresses_without_contracts)
+    print('\nNo Prior Activity:\n', addresses_with_no_prior_activity, '\n')
+    print('\nAddresses without contract creation:\n', addresses_without_contracts, '\n')
 
-    # Ensure 'fixedfloat' directory exists
-    FileManager.create_dir('fixedfloat')
+    # Write the filtered addresses to a SQLite database
+    db = DBManager(db_name='kendhalaltay', user='kendhalaltay')
+    for address in addresses_without_contracts:
+        db.insert_address(address)
+    db.close_connection()
 
-    # Write the filtered addresses to a file
-    FileManager.write_to_file('fixedfloat/filtered_addresses.json', addresses_without_contracts)
+    print("Addresses written to database\n")
 
-    print("Addresses written to 'fixedfloat/filtered_addresses.json'")
+
+def job():
+    main()
 
 
 if __name__ == "__main__":
-    main()
+    schedule.every(1).minutes.do(job)
+    schedule.every(1).minutes.do(check_swaps)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
--- wallet_link.py
+++ wallet_link.py
@@ -1,6 +1,7 @@
 import requests
 import yaml
 
+
 # Upload the config file
 file = open(f"credentials.yml", 'r')
 config = yaml.load(file, Loader=yaml.FullLoader)
@@ -17,7 +18,7 @@ def get_transactions(address):
     return data['result'] if 'result' in data else []
 
 
-def find_hops(start_address, target_addresses, max_hops=5, max_transactions=100):
+def find_hops(start_address, target_addresses, max_hops=2, max_transactions=100):
     visited = set()
     queue = [(start_address.lower(), [], None, 0)]
     enqueued = set([start_address.lower()])
@@ -42,42 +43,108 @@ def find_hops(start_address, target_addresses, max_hops=5, max_transactions=100)
 
 def main():
     start_addresses = {
-        '0x50eD4D3e48B27681371b0c9F375Eb12a85e241Dc': 'TEST_1'
+        '0x18d044d8c82360c5834e220e8c1ad624fb7b9e03': 'PAI',
+        '0xd7d82568bd2cdaa4d8a1049c535ab8e6827728c1': 'PAI',
+        '0xc18b0fbd997dc057acd5d19c0bd62e93bed14c49': 'PAI',
+        '0xbab7fc41e5be0bd9ab4c7de22062892baa7efb41': 'PAI',
+        '0x8f92604ad3986c9525714d4b5537bef96be3374b': 'PAI',
+        '0x36e0e846de855be68668b3bdbc7431334c59e482': 'PAI',
+        '0x3a25e5a551475320c119319a56ad756ae63241d4': 'PAI',
+        '0xfbb4e3cf04ed6588e64563aa0dbf2e958118c0ed': 'PAI',
+        '0x7aab0681c4c53fc22638e4597d4a5ed3038fafdd': 'PAI',
+        '0x40b1762268218b8cd0e8ee3615d6f8ed44f797c8': 'PAI',
+        '0x2d2f29d6c07fef876b481ad0556e7c058220e041': 'PAI',
+        '0xcd0e05ac572e56bf278729c41793ad88424db4ef': 'PAI',
+        '0x9f1652df4c46616702a0005795bb4bb32a65f6ea': 'PAI',
+        '0xdf839f2d5bbde84d6534e828d2f9ad8923a2d0c5': 'PAI',
+        '0xa32f583c7f5bc0d9c06633c2f3cfc338e78d12bd': 'PAI',
+        '0xd4c0b06ee3df6c2b93b8cc2e118c5b093905aa6c': 'PAI',
+        '0x4734798424bffcf252d46b2e4af91dc06bdbf79c': 'PAI',
+        '0xc9163a208fae2f99125587b0b232f2dd3de94e5a': 'PAI',
+        '0x1ce754ab1a8cf61892d803aea9ee1a2b348195e7': 'PAI',
+        '0x252c76dba68aa739974d860ca4d7dc844abe4707': 'PAI',
+        '0x8c5c7fc9140c1bcb12b18becbbf29ff3e537f9c1': 'PAI',
+        '0xf453e3b803ee9bc07172b80ddf384cd00e8d489f': 'PAI',
+        '0x60b8d1badbf1a3fdf34c552de8e1d7189c2bc807': 'PAI',
+        '0xe3e22035eb0b40a8fe0422e9eaf4768f9b44eb28': 'PAI',
+        '0x6931dc457303d13d0a05cd31ee751f0f518dad88': 'PAI',
+        '0x5ba30bde3c747579acbeec21094c4397ade467d2': 'PAI',
+        '0x78263c287f840f6ed12c7d779591a90b5619a009': 'PAI',
+        '0x9541631e8079e294f09031757e3229046bda90f0': 'PAI',
+        '0x00be25bd6d560f48379a309d010c997745ead382': 'PAI',
+        '0x1eba5bdc74b9421823c11fece470a2177a6b5ead': 'PAI',
+        '0xd7286234d05a3746f2fcd083d9e218ce2268db77': 'PAI',
+        '0x358e21d9601b31d52390943fccb72c197beb3ea3': 'PAI',
+        '0x8f3bb68716856373a2615155417fe92f2f556989': 'PAI',
+        '0xf959df6a6427a4920dd6c0ac9484e3ee0b6f08d0': 'PAI',
+        '0x43071f98ee53439e14816af7c2f2cc59733d130d': 'PAI',
+        '0xeb3ba847938ebe91c66f79a20062ba0f24f31920': 'PAI',
+        '0x9d94e8b052a80f2f013c43e4e8d4a55dee4806d9': 'PAI',
+        '0xca9baa26f5a7386494371b217cb8629e30c9bcb8': 'PAI',
+        '0xeef69cf172a9f546dc808e5eef0c66e0fe3b7582': 'PAI',
+        '0xa45614f1b3078063fe84794319c3e6860a048707': 'PAI',
+        '0xf87c00900176804708ea2909af86c561e6bcb021': 'PAI',
+        '0xdec8d8fa51fa161700175a86ca1f068e669fefb7': 'PAI',
+        '0xd493fdb7ad5970b796b7d5459b0f6db617268a5e': 'PAI',
+        '0x289a4dc7401927fcc12aac836403730835ad0065': 'PAI',
+        '0xf037c76c285d7d1771f8e014ab0a7f647e845b33': 'PAI',
+        '0x8862a6c968625f24d5e35a82eee6ab16305f3016': 'PAI',
+        '0xad6600b8177ce88f1b8fd2c1d6eea93c9f27aa85': 'PAI',
+        '0xfdef6c90eaae50f0cea69bb1108411d2e7d6a13d': 'PAI',
+        '0xf9cdd61e08cee88deb31a5127cee26da30b97c60': 'PAI',
+        '0xe780d5f1d8e0153edd5684ad0e0be8a987a2cf0f': 'PAI',
     }
 
     start_addresses = {address.lower(): name for address, name in start_addresses.items()}
 
     for start_address, start_name in start_addresses.items():
         target_addresses = {
-            '0xc9E170d9C62b7765F624459C3fdDf23de9f4CeC3': 'CTM',
-            '0xd6BDF425640032b949aeB2130a9ACB9a3181B58b': 'WJS',
-            '0x50eD4D3e48B27681371b0c9F375Eb12a85e241Dc': "TEST_1",
-            '0x58203347923ef9751748D098084611FF4473d1Cd': 'TEST_4',
-            '0xbdb4BeeF21efC8AE04A6Bf11e685954BEc015125': 'TEST_5',
-            '0x80C94d637F5F51758D5935284F3B3091ceEf2a8C': 'TEST_6',
-            '0x7322f9932d68FB99a84fb9F89375c8cB7EBb9bB9': 'TEST_7',
-            '0x32Fd0f2853dd29b479c4879D7683d81e5C3cC3c1': 'TEST_8',
-            '0x4730d108aC076973373155932a465ed438C7b1a2': 'TEST_9',
-            '0x82ea7840441c4B4E16Ce4A9c58364dc7FBe19048': 'TEST_10',
-            '0x861193dF5007fDE8DC8F2B72dB746DFf226Cea68': 'TEST_11',
-            '0x743CB013D459c358ed571A21cC105E35c284C385': 'TEST_12',
-            '0x9822E23558c2837a499541Bf22433B0F820F213A': 'TEST_13',
-            '0x5440cC69CB31CA46decf34C246FC395378D731b4': 'TEST_14',
-            '0xC78d8F4493B5A1455152DE575deE50D986871eC9': 'TEST_15',
-            '0xc863E595C3b56F142CF71682c74B25F719EE85E2': 'POE_1',
-            '0xF9c336825Ebb7C8fC2c96856912364d8a35E8145': 'POE_10',
-            '0x234a3bA2dd72d82853974aB185439761Ec391db7': 'POE_11',
-            '0xeDAf179d5436dE9eF5e0052F3E13De963976c5B2': 'POE_12',
-            '0x3641677dB54a774B8C2be96268aBf7a052E15985': 'POE_13',
-            '0xa9A6747E4B17122359684C8D6c491A0098d832F8': 'POE_14',
-            '0x11238CD54755895718d03880b6d28D04aB628263': 'POE_16',
-            '0xac617d90C4ce370fC7bCb216d66eA6f809830f82': 'POE_17',
-            '0x3EB109Be82C6C20f5dbbF91dbc19Dd22C4550AaF': 'POE_18',
-            '0x514854B5E7BF04Fd9996483bB028cb054bE19637': 'POE_2',
-            '0x69889BEA959641b8ef932C3C969810bEe21E5AfA': 'POE_3',
-            '0xA7CE0C0BDeC1ce84575daBECB38F362f171B6235': 'POE_4',
-            '0xa484F156526f2377Be2f93327640F45B6BDcB8f1': 'POE_5',
-            '0x50171E875BC1Af9Af749fE88057220675d107013': 'POE_6',
+            # '0xc9E170d9C62b7765F624459C3fdDf23de9f4CeC3': 'CTM',
+            # '0xd6BDF425640032b949aeB2130a9ACB9a3181B58b': 'WJS',
+            # '0x50eD4D3e48B27681371b0c9F375Eb12a85e241Dc': "TEST_1",
+            # '0x58203347923ef9751748D098084611FF4473d1Cd': 'TEST_4',
+            # '0xbdb4BeeF21efC8AE04A6Bf11e685954BEc015125': 'TEST_5',
+            # '0x80C94d637F5F51758D5935284F3B3091ceEf2a8C': 'TEST_6',
+            # '0x7322f9932d68FB99a84fb9F89375c8cB7EBb9bB9': 'TEST_7',
+            # '0x32Fd0f2853dd29b479c4879D7683d81e5C3cC3c1': 'TEST_8',
+            # '0x4730d108aC076973373155932a465ed438C7b1a2': 'TEST_9',
+            # '0x82ea7840441c4B4E16Ce4A9c58364dc7FBe19048': 'TEST_10',
+            # '0x861193dF5007fDE8DC8F2B72dB746DFf226Cea68': 'TEST_11',
+            # '0x743CB013D459c358ed571A21cC105E35c284C385': 'TEST_12',
+            # '0x9822E23558c2837a499541Bf22433B0F820F213A': 'TEST_13',
+            # '0x5440cC69CB31CA46decf34C246FC395378D731b4': 'TEST_14',
+            # '0xC78d8F4493B5A1455152DE575deE50D986871eC9': 'TEST_15',
+            # '0xc863E595C3b56F142CF71682c74B25F719EE85E2': 'POE_1',
+            # '0xF9c336825Ebb7C8fC2c96856912364d8a35E8145': 'POE_10',
+            # '0x234a3bA2dd72d82853974aB185439761Ec391db7': 'POE_11',
+            # '0xeDAf179d5436dE9eF5e0052F3E13De963976c5B2': 'POE_12',
+            # '0x3641677dB54a774B8C2be96268aBf7a052E15985': 'POE_13',
+            # '0xa9A6747E4B17122359684C8D6c491A0098d832F8': 'POE_14',
+            # '0x11238CD54755895718d03880b6d28D04aB628263': 'POE_16',
+            # '0xac617d90C4ce370fC7bCb216d66eA6f809830f82': 'POE_17',
+            # '0x3EB109Be82C6C20f5dbbF91dbc19Dd22C4550AaF': 'POE_18',
+            # '0x514854B5E7BF04Fd9996483bB028cb054bE19637': 'POE_2',
+            # '0x69889BEA959641b8ef932C3C969810bEe21E5AfA': 'POE_3',
+            # '0xA7CE0C0BDeC1ce84575daBECB38F362f171B6235': 'POE_4',
+            # '0xa484F156526f2377Be2f93327640F45B6BDcB8f1': 'POE_5',
+            # '0x50171E875BC1Af9Af749fE88057220675d107013': 'POE_6',
+
+            # '0xFACaEf557877B64a11fd73494797fcD2C0BD4f7F': 'ERIC_14',
+            # '0x2eBD5Bd5053D17705102f101fAd034E3BEb8f424': 'ERIC_13',
+            # '0xB97A925d8027E8633c2aADd377260F9d0126F7f1': 'ERIC_9',
+            # '0xE7d6a4Cd223Ab93cBBa3527C1A9bdd280b385ab3': 'ERIC_16',
+            # '0xB89b0f2DE40F5775e609d09A3a768d3e12b8B812': 'ERIC_55',
+
+            '0x4ad434b8CDC3AA5AC97932D6BD18b5d313aB0f6f': 'EVERMOON',
+            '0x590f00eDc668D5af987c6076c7302C42B6FE9DD3': 'SCAM',
+            '0x370DE5fEeb723a92d8ef7d269620Ea3736268520': 'AUDITUS',
+            '0xE08eF9206a8a7C9337cC6611b4f5226Fdafc4772': 'MESSI',
+
+            '0x5D39957Fc88566F14AE7E8aB8971d7c603f0ce5e': 'EYE',
+            '0x3db045814D0a29d831fe38055CB97a956eF7cAfb': 'REMIT',
+            '0x85225Ed797fd4128Ac45A992C46eA4681a7A15dA': 'HYPE',
+            '0x3486b751a36F731A1bEbFf779374baD635864919': 'INEDIBLE',
+            '0xF68415bE72377611e95d59bc710CcbBbf94C4Fa2': 'AAI',
         }
         target_addresses = {address.lower(): name for address, name in target_addresses.items()}
         paths = find_hops(start_address, target_addresses)
